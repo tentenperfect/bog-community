58284c00f49f37f2ef82aaf22cf8a1a640c86d32
xutenan <xtn_1124@163.com>
2022-07-13 16:33:02 +0800

    添加全局的时间转换指令

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import timeFormatter from '@/util/timeFormatter'
+const app = createApp(App)
 
-createApp(App).use(store).use(router).mount('#app')
+// 自定义指令(全局指令)
+app.directive('focus', {
+    // 当被绑定的元素挂载到 DOM 中时……
+    mounted(el:HTMLElement) {
+      // 聚焦元素
+      el.focus()
+    }
+})
+timeFormatter(app)
+app.use(ElementPlus).use(store).use(router).mount('#app')
+
+
