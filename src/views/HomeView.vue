fb84953071dc91711ef2bfa6f08afc32ca77f254
xutenan <xtn_1124@163.com>
2022-07-13 16:34:45 +0800

    对时间转换指令进行测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <el-button type="primary">Primary</el-button>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    <input type="text" placeholder="我是input输入框" v-focus>
+    <span v-timeformatter>{{timestamp}}</span>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'HomeView',
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      direction: 'right',
+      timestamp: 1624452193
+    }
   },
-});
+  methods: {
+  },
+//   directives: {
+//     focus: {
+//       // 指令的定义
+//       mounted(el) {
+//         el.focus()
+//       }
+//     }
+// }
+
+})
 </script>
+<style lang="less">
+</style>
